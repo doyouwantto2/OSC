8f10e39959f65b033b9080fa422d983fe27dc446
doyouwantto2 <tinhphong2580@gmail.com>
2025-07-05 12:46:36 +0700

    d

--- system/desktop/basic.nix
+++ system/desktop/basic.nix
@@ -4,7 +4,6 @@
   environment.systemPackages = with pkgs; [
     wl-clipboard
     git
-    ripgrep
   ]; 
 }
 
--- system/programming/text-editor.nix
+++ system/programming/text-editor.nix
@@ -3,6 +3,17 @@
 {
   environment.systemPackages = with pkgs; [
     vscodium
+
     neovim
+    ripgrep
+    fd
+    sqlite
+    lazygit
+    imagemagick
+    ghostscript
+    tectonic
+    graphviz
+    mermaid-cli
+    texlive.combined.scheme-full
   ]; 
 }

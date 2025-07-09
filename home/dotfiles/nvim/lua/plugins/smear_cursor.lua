f450cd65ad04e0bc29203356bd75986d2b59bb7d
doyouwantto2 <tinhphong2580@gmail.com>
2025-07-09 19:01:05 +0700

    d

--- home/dotfiles/nvim/lua/plugins/smear_cursor.lua
+++ home/dotfiles/nvim/lua/plugins/smear_cursor.lua
@@ -6,12 +6,11 @@ return {
     smear_between_neighbor_lines = true,
     scroll_buffer_space = true,
     legacy_computing_symbols_support = false,
-    smear_insert_mode = true,
 
     cursor_color = "#f5f5f5",
-    stiffness = 0.4,
-    trailing_stiffness = 0.2,
-    damping = 0.7,
+    stiffness = 0.6
+    trailing_stiffness = 0.5,
+    damping = 0.6,
     trailing_exponent = 5,
     never_draw_over_target = true,
     hide_target_hack = true,

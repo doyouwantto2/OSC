8f29813165b536baf46466f4a9499b64e793aaea
doyouwantto2 <tinhphong2580@gmail.com>
2025-07-09 18:45:56 +0700

    remove cursor ai

--- home/dotfiles/nvim/lua/plugins/smear_cursor.lua
+++ home/dotfiles/nvim/lua/plugins/smear_cursor.lua
@@ -1,6 +1,6 @@
 return {
   "sphamba/smear-cursor.nvim",
-
+  event = "VeryLazy",
   opts = {
     smear_between_buffers = true,
     smear_between_neighbor_lines = true,
@@ -8,7 +8,7 @@ return {
     legacy_computing_symbols_support = false,
     smear_insert_mode = true,
 
-    cursor_color = "#f5f5f5",
+    cursor_color = "#f5f5f5", -- light gray/white
     stiffness = 0.3,
     trailing_stiffness = 0.1,
     damping = 0.5,
@@ -17,4 +17,15 @@ return {
     hide_target_hack = true,
     gamma = 1,
   },
+  config = function(_, opts)
+    require("smear-cursor").setup(opts)
+
+    -- Make the cursor effect trail more transparent
+    vim.api.nvim_set_hl(0, "SmearCursor", {
+      fg = opts.cursor_color or "#f5f5f5",
+      bg = "NONE", -- transparent background
+      blend = 70,  -- transparency level (0 = opaque, 100 = invisible)
+    })
+  end,
 }
+

63d506885cedcc02b11fd75e4191a2bd311b5af9
doyouwantto2 <tinhphong2580@gmail.com>
2025-07-09 13:38:02 +0700

    d

--- home/dotfiles/wezterm/wezterm.lua
+++ home/dotfiles/wezterm/wezterm.lua
@@ -2,15 +2,7 @@ local wezterm = require("wezterm")
 local config = {}
 local act = wezterm.action
 
-function scheme_for_appearance(appearance)
-  if appearance:find "Dark" then
-    return "Catppuccin Mocha"
-  else
-    return "Catppuccin Latte"
-  end
-end
-
-config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.color_scheme = "rebecca"
 config.window_background_opacity = 0.8
 config.warn_about_missing_glyphs = false
 config.enable_wayland = true

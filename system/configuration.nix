0d0fb21865ed3c4c4c0e5690e9c4d1cbf2370598
doyouwantto2 <tinhphong2580@gmail.com>
2025-07-09 11:44:35 +0700

    d

--- system/configuration.nix
+++ system/configuration.nix
@@ -64,14 +64,14 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
+  sound.enable = true;
   services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    #jack.enable = true;
+    jack.enable = true;
     wireplumber.enable = true;
   };
 

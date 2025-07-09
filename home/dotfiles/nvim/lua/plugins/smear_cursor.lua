b3bd90c99605165daabcc5cf40c0f2b017c7f9ce
doyouwantto2 <tinhphong2580@gmail.com>
2025-07-09 18:39:31 +0700

    add cursor

---
+++ home/dotfiles/nvim/lua/plugins/smear_cursor.lua
@@ -0,0 +1,20 @@
+return {
+  "sphamba/smear-cursor.nvim",
+
+  opts = {
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+    scroll_buffer_space = true,
+    legacy_computing_symbols_support = false,
+    smear_insert_mode = true,
+
+    cursor_color = "#f5f5f5",
+    stiffness = 0.3,
+    trailing_stiffness = 0.1,
+    damping = 0.5,
+    trailing_exponent = 5,
+    never_draw_over_target = true,
+    hide_target_hack = true,
+    gamma = 1,
+  },
+}

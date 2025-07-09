c67f176dace2e57c67e912eec4f167051e75863d
doyouwantto2 <tinhphong2580@gmail.com>
2025-07-10 00:13:32 +0700

    d

--- home/dotfiles/nvim/lua/plugins/disable_mason.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { "williamboman/mason.nvim", enabled = false },
-  { "jay-babu/mason-null-ls.nvim", enabled = false },
-  { "jay-babu/mason-nvim-dap.nvim", enabled = false },
-  { "WhoIsSethDaniel/mason-tool-installer.nvim", enabled = false },
-}
-

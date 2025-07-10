c2061920aa7115b08a2961c4926cfefd8087d247
doyouwantto2 <tinhphong2580@gmail.com>
2025-07-10 22:07:01 +0700

    change keybinding in nvim

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -1,2 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- normal mode mappings
+vim.keymap.set("n", "<C-l>", "e", { noremap = true })
+vim.keymap.set("n", "<C-h>", "b", { noremap = true })
+vim.keymap.set("n", "<C-j>", "<C-d>", { noremap = true })
+vim.keymap.set("n", "<C-k>", "<C-u>", { noremap = true })
+
+-- disable original keys
+vim.keymap.set("n", "e", "<Nop>")
+vim.keymap.set("n", "b", "<Nop>")
+vim.keymap.set("n", "<C-d>", "<Nop>")
+vim.keymap.set("n", "<C-u>", "<Nop>")
+
+-- visual mode (make sure movement applies properly)
+vim.keymap.set("v", "<C-l>", ":normal! e<CR>", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-h>", ":normal! b<CR>", { noremap = true, silent = true })
+

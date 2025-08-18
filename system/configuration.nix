800f472c0152515b411b94c95a7dbd940ddf782b
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-08-18 15:59:29 +0700

    d

--- system/configuration.nix
+++ system/configuration.nix
@@ -102,7 +102,10 @@
     enable = true;
     ensureDatabases = [ "mydatabase" ];
     enableTCPIP = true;
-    port = 5432;
+    settings = {
+      ssl = true;
+      port = 5432;
+    };
     authentication = pkgs.lib.mkOverride 10 ''
       #type database DBuser origin-address auth-method
       local all      all     trust

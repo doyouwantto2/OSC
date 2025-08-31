fb002d48cb6c948849111c673aba57fff68c43d4
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-08-31 19:18:22 +0700

    fix syntax

--- home/home.nix
+++ home/home.nix
@@ -9,7 +9,6 @@
     ".config/cava".source = ./dotfiles/cava;
     ".config/wezterm".source = ./dotfiles/wezterm;
     ".config/starship".source = ./dotfiles/starship;
-    ".config/assets".source = ./dotfiles/assets;
     #".config/hypr".source = ./dotfiles/hypr;
     #".config/eww".source = ./dotfiles/eww;
 

7cb3881fe3a04f4df5a9cca0c3d411fda0e7f893
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-09-03 22:50:38 +0700

    use flake to install qt6 modules

--- system/programming/languages.nix
+++ system/programming/languages.nix
@@ -65,18 +65,6 @@
     hyprlang
     hyprls
 
-    qt6.qtbase
-    qt6.qtdeclarative
-    qt6.qtmultimedia
-    qt6.qttools
-    qt6.qtsvg
-    qt6.qtwayland
-    qt6.qtspeech
-    qt6.qtpositioning
-    qt6.qtvirtualkeyboard
-    qt6.qtwebsockets
-    qt6.qtquick3d
-
   ];
 
 }

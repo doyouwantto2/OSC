08539264af2dd66912b4fc08b42594f06f46b7a3
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-09-03 22:52:04 +0700

    fix

--- system/desktop/workspace/front-end.nix
+++ system/desktop/workspace/front-end.nix
@@ -6,20 +6,20 @@
 
   environment.systemPackages = with pkgs; [
     waypaper
-    quickShell.withModules
-    [
-      qt6.qtbase
-      qt6.qtdeclarative
-      qt6.qtmultimedia
-      qt6.qttools
-      qt6.qtsvg
-      qt6.qtwayland
-      qt6.qtspeech
-      qt6.qtpositioning
-      qt6.qtvirtualkeyboard
-      qt6.qtwebsockets
-      qt6.qtquick3d
-    ]
+    (quickShell.withModules
+      [
+        qt6.qtbase
+        qt6.qtdeclarative
+        qt6.qtmultimedia
+        qt6.qttools
+        qt6.qtsvg
+        qt6.qtwayland
+        qt6.qtspeech
+        qt6.qtpositioning
+        qt6.qtvirtualkeyboard
+        qt6.qtwebsockets
+        qt6.qtquick3d
+      ])
 
     hyprcursor
     hyprpaper

79555dce826b0d9bd47701bdfae2006a55b13f0e
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-09-03 23:10:09 +0700

    add qt6 full

--- system/desktop/workspace/front-end.nix
+++ system/desktop/workspace/front-end.nix
@@ -1,41 +1,16 @@
 { config, pkgs, quickShell, ... }:
-let
-  qmlEnv = pkgs.qt6.qtdeclarative.withModules (m: with m; [
-    qtbase
-    qtdeclarative
-    qtmultimedia
-    qttools
-    qtsvg
-    qtwayland
-    qtspeech
-    qtpositioning
-    qtvirtualkeyboard
-    qtwebsockets
-    qtquick3d
-  ]);
-in
+
 {
   programs.hyprland.enable = true;
   programs.hyprlock.enable = true;
 
   environment.systemPackages = with pkgs; [
     waypaper
-
     (quickShell.withModules
       [
-        qt6.qtbase
-        qt6.qtdeclarative
-        qt6.qtmultimedia
-        qt6.qttools
-        qt6.qtsvg
-        qt6.qtwayland
-        qt6.qtspeech
-        qt6.qtpositioning
-        qt6.qtvirtualkeyboard
-        qt6.qtwebsockets
-        qt6.qtquick3d
+        qt6.full
       ])
-    qmlEnv
+    qt6.full
 
     hyprcursor
     hyprpaper

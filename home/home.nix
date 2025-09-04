c4236dea8ad7fc36578993edbd6fb6ad82b00f9a
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-09-04 22:22:51 +0700

    add more symlink to assets

--- home/home.nix
+++ home/home.nix
@@ -15,6 +15,7 @@
     # For extra things
     "Documents/Books".source = ./extra/books;
     "Pictures/Assets".source = ./extra/assets;
+    ".local/state/caelestia/wallpaper/Assets".source = ./extra/assets;
   };
 
 

133117c41c964098b21898bf378cc1f570cfc932
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-09-04 23:42:02 +0700

    move neovim back to home

--- home/home.nix
+++ home/home.nix
@@ -5,12 +5,11 @@
 
   home.file = {
     # For dotfiles
-    #".config/nvim".source = ./dotfiles/nvim;
+    ".config/nvim".source = ./dotfiles/nvim;
     ".config/cava".source = ./dotfiles/cava;
     ".config/wezterm".source = ./dotfiles/wezterm;
     ".config/starship".source = ./dotfiles/starship;
     #".config/hypr".source = ./dotfiles/hypr;
-    #".config/eww".source = ./dotfiles/eww;
 
     # For extra things
     "Documents/Books".source = ./extra/books;

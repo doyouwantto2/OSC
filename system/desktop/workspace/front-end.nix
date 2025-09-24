a5f67d3677b3f15621d2ae9bcdfffb0aa7e8a032
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-09-24 16:22:29 +0700

    remove hyprcursor

--- system/desktop/workspace/front-end.nix
+++ system/desktop/workspace/front-end.nix
@@ -4,14 +4,11 @@
   programs.hyprland.enable = true;
   programs.hyprlock.enable = true;
 
-  environment.systemPackages = with user; [
+  environment.systemPackages = [
     ags.packages.${user.system}.agsFull
     astal.packages.${user.system}.io
     astal.packages.${user.system}.astal3
     astal.packages.${user.system}.astal4
-
-    hyprcursor
-    hyprpaper
   ];
 }
 

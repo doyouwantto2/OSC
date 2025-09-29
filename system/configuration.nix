527586bf5b773b2609e2183e272ae86622c57171
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-09-29 15:03:43 +0700

    remove auto login

--- system/configuration.nix
+++ system/configuration.nix
@@ -95,8 +95,8 @@
   };
 
   # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "emiya2467";
+  # services.displayManager.autoLogin.enable = true;
+  # services.displayManager.autoLogin.user = "emiya2467";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;

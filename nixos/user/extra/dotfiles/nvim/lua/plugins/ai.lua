7df163a10955f69c43fe9b4f1c67865134ace619
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-11-15 21:53:04 +0700

    switch to surf

--- nixos/user/extra/dotfiles/nvim/lua/plugins/ai.lua
+++ nixos/user/extra/dotfiles/nvim/lua/plugins/ai.lua
@@ -1,1 +1,10 @@
-return {}
+return {
+  "Exafunction/windsurf.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    require("codeium").setup({})
+  end,
+}

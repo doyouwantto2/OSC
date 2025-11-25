edcddc3af236ca4e38090c4ce4e1185b6933a27c
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-11-25 09:54:52 +0700

    update

--- nixos/user/shrc.nix
+++ nixos/user/shrc.nix
@@ -30,9 +30,29 @@
       '';
 
       interactiveShellInit = ''
+        # Get date components
+        set _hour (date +"%H")
+        set _min  (date +"%M")
+        set _sec  (date +"%S")
+        set _year (date +"%Y")
+        set _month (date +"%m")
+        set _day   (date +"%d")
+        set _weekday (date +"%a")
+
+        # Choose color based on hour
+        if test $_hour -ge 6 -a $_hour -lt 12
+          set _color (set_color yellow)
+        else if test $_hour -ge 12 -a $_hour -lt 18
+          set _color (set_color green)
+        else if test $_hour -ge 18 -a $_hour -lt 22
+          set _color (set_color magenta)
+        else
+          set _color (set_color cyan)
+        end
+
+        # Set greeting
         set fish_greeting \
-          (set_color green)(whoami)(set_color cyan)"!"(set_color normal)"  —  " \
-          (set_color yellow)(date +"%Y-%m-%d (%A) %H:%M:%S")(set_color normal)
+          $_color"$_hour:$_min:$_sec  $_year-$_month-$_day ($_weekday)"(set_color normal)
       '';
 
       shellAliases = {

a8ac828e4387c88a022703f7b44ea7ab9ce6d085
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-11-25 09:57:42 +0700

    update

--- nixos/user/shrc.nix
+++ nixos/user/shrc.nix
@@ -30,29 +30,18 @@
       '';
 
       interactiveShellInit = ''
-        # Get date components
-        set _hour (date +"%H")
-        set _min  (date +"%M")
-        set _sec  (date +"%S")
-        set _year (date +"%Y")
-        set _month (date +"%m")
-        set _day   (date +"%d")
-        set _weekday (date +"%a")
+        # Get username and date
+        set _user (whoami)
+        set _date (date +"%Y-%m-%d")
+        set _weekday (date +"(%a)")
 
-        # Choose color based on hour
-        if test $_hour -ge 6 -a $_hour -lt 12
-          set _color (set_color yellow)
-        else if test $_hour -ge 12 -a $_hour -lt 18
-          set _color (set_color green)
-        else if test $_hour -ge 18 -a $_hour -lt 22
-          set _color (set_color magenta)
-        else
-          set _color (set_color cyan)
-        end
-
-        # Set greeting
+        # Build the greeting with colors
         set fish_greeting \
-          $_color"$_hour:$_min:$_sec  $_year-$_month-$_day ($_weekday)"(set_color normal)
+          (set_color --bold orange)$_user(set_color normal) \
+          "-" \
+          (set_color --bold green)$_date(set_color normal) \
+          " " \
+          (set_color --bold cyan)$_weekday(set_color normal)
       '';
 
       shellAliases = {

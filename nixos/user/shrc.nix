2f6d955d38b6a7363256e4357c3a259bab15cb38
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-11-25 09:38:28 +0700

    update

--- nixos/user/shrc.nix
+++ nixos/user/shrc.nix
@@ -29,6 +29,12 @@
         zoxide init fish | source
       '';
 
+      interactiveShellInit = ''
+        set fish_greeting \
+          (set_color cyan)"Welcome, "(whoami)(set_color normal)"! Today is " \
+          (set_color yellow)(date +'%A, %d %B %Y')(set_color normal)
+      '';
+
       shellAliases = {
         systemSync = "sudo nixos-rebuild switch --flake .";
         userSync = "nix run home-manager -- switch --flake .";

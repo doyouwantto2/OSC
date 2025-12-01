182914fd4151308b73974335e60cc1b633bb10d9
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-12-01 10:43:39 +0700

    update

--- nixos/user/user.nix
+++ nixos/user/user.nix
@@ -10,7 +10,7 @@
   home.username = user.name;
   home.homeDirectory = "/home/${user.name}";
 
-  home.stateVersion = "25.05";
+  home.stateVersion = "25.11";
 
   programs.home-manager.enable = true;
 }

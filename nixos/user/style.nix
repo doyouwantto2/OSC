8281345d069f1bcb1b6b9b440197fb0b9eaab661
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-12-12 03:35:14 +0700

    update

--- nixos/user/style.nix
+++ nixos/user/style.nix
@@ -2,18 +2,13 @@
 
 {
   stylix.enable = true;
-  stylix.image = ./extra/wallpapers/GraveYard.png;
+  stylix.image = ./extra/wallpapers/GloriousEva.png;
   stylix.polarity = "dark";
 
   # GNOME
   stylix.targets.gnome.enable = true;
   stylix.targets.gnome.useWallpaper = true;
-  stylix.targets.gnome.cursor = {
-    enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Ice";
-    size = 24; # optional
-  };
+  stylix.targets.gnome.fonts.enable = true;
 
   # GTK
   stylix.targets.gtk.enable = true;
@@ -24,4 +19,25 @@
   stylix.targets.firefox.profileNames = [ "default-release" ];
   stylix.targets.firefox.colorTheme.enable = true;
   stylix.targets.firefox.firefoxGnomeTheme.enable = true;
+
+  # Btop
+  stylix.targets.btop.enable = true;
+  stylix.targets.btop.opacity.enable = true;
+  stylix.targets.btop.colors.enable = true;
+
+  # Vesktop
+  stylix.targets.vesktop.enable = true;
+  stylix.targets.vesktop.colors.enable = true;
+  stylix.targets.vesktop.fonts.enable = true;
+
+  # Lazygit
+  stylix.targets.lazygit.enable = true;
+  stylix.targets.lazygit.colors.enable = true;
+
+  # Fzf
+  stylix.targets.fzf.enable = true;
+  stylix.targets.fzf.colors.enable = true;
+
+  # Micro
+  stylix.targets.micro.enable = true;
 }

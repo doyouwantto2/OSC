c5941bb955adbd0e326d8b66815b4948dd0e5268
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-12-12 10:18:55 +0700

    update

--- nixos/system/desktop/workflow/frontend.nix
+++ nixos/system/desktop/workflow/frontend.nix
@@ -4,7 +4,6 @@
   environment.systemPackages = [
     quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default
     pkgs.qt6.qtdeclarative
-    pkgs.qt6.qtquickcontrols2
     pkgs.qt6.qtwayland
     pkgs.qt6.qttools
     pkgs.qt6.qtsvg

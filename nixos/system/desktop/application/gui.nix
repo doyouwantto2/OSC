bff3787f38196692b6df31b7e53e19a6f771b24c
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-12-12 16:05:06 +0700

    update

--- nixos/system/desktop/application/gui.nix
+++ nixos/system/desktop/application/gui.nix
@@ -4,7 +4,7 @@
   environment.systemPackages = with pkgs; [
     brave
 
-    rofi-wayland
+    walker
     blueman
     waypaper
 
--- nixos/user/file.nix
+++ nixos/user/file.nix
@@ -5,7 +5,6 @@
     "${config.xdg.configHome}/nvim".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/nvim;
     "${config.xdg.configHome}/cava".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/cava;
     "${config.xdg.configHome}/wezterm".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/wezterm;
-    "${config.xdg.configHome}/rofi".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/rofi;
 
     "${config.xdg.configHome}/hypr".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/hypr;
     "${config.xdg.configHome}/starship.toml".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/starship.toml;

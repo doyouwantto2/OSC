371b5edbf7991505dda8a5d63f6a7a16339b8f3e
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-12-13 13:06:52 +0700

    update

--- nixos/system/desktop/workflow/frontend.nix
+++ nixos/system/desktop/workflow/frontend.nix
@@ -4,13 +4,17 @@
   environment.systemPackages = with pkgs; [
     quickshell
     noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+
     qt6.qtdeclarative
     qt6.qtwayland
     qt6.qttools
     qt6.qtsvg
     qt6.qtimageformats
     qt6.qtmultimedia
+
     qt6.qt5compat
+    libsForQt5.qt5.qtgraphicaleffects
+
     papirus-icon-theme
   ];
 }

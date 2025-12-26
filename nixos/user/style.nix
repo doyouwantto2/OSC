c4a5e843c7233dbd540cd0b3585dc4634266716c
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-12-26 22:20:03 +0700

    update

--- nixos/user/style.nix
+++ nixos/user/style.nix
@@ -15,11 +15,12 @@
   stylix.targets.gtk.enable = true;
   stylix.targets.gtk.flatpakSupport.enable = true;
 
-  # Firefox
-  stylix.targets.firefox.enable = true;
-  stylix.targets.firefox.profileNames = [ "default-release" ];
-  stylix.targets.firefox.colorTheme.enable = true;
-  stylix.targets.firefox.firefoxGnomeTheme.enable = true;
+  # Librewolf
+  stylix.targets.librewolf.enable = true;
+  stylix.targets.librewolf.fonts.enable = true;
+  stylix.targets.librewolf.inputs.enable = true;
+  stylix.targets.librewolf.firefoxGnomeTheme.enable = true;
+  stylix.targets.librewolf.profileNames = [ "default-release" ];
 
   # Vesktop
   stylix.targets.vesktop.enable = true;

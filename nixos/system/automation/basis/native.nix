240da3c83fc8010bc34ab2b390f0fd05955dbc28
doyouwantto2 <tinhphong2580@gmail.com>
2026-01-26 21:56:55 +0700

    update

--- nixos/system/automation/basis/native.nix
+++ nixos/system/automation/basis/native.nix
@@ -36,6 +36,7 @@
 
   environment.sessionVariables = {
     OPENSSL_DIR = "${pkgs.openssl.dev}";
+    EDITOR = "nvim";
     OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
     OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";

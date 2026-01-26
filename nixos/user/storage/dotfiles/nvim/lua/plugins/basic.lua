e18bd094a7bf8f9775b24cc10334cd53a5361f85
doyouwantto2 <tinhphong2580@gmail.com>
2026-01-27 00:04:50 +0700

    update

--- nixos/user/storage/dotfiles/nvim/lua/plugins/basic.lua
+++ nixos/user/storage/dotfiles/nvim/lua/plugins/basic.lua
@@ -2,4 +2,5 @@ return {
   { "mason-org/mason.nvim", enabled = false },
   { "mason-org/mason-lspconfig.nvim", enabled = false },
   { "WhoIsSethDaniel/mason-tool-installer.nvim", enabled = false },
+  { "folke/snacks.nvim", optional = true, opts = { dashboard = { enabled = false } } },
 }

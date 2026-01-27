0efdd25a9f966da70482ad52d34abe395c966e71
doyouwantto2 <tinhphong2580@gmail.com>
2026-01-27 18:52:32 +0700

    update

--- nixos/user/storage/storage.nix
+++ nixos/user/storage/storage.nix
@@ -2,23 +2,17 @@
 
 {
   home.file = {
-    "${config.xdg.configHome}/nvim".source =
-      config.lib.file.mkOutOfStoreSymlink ./dotfiles/nvim;
-    "${config.xdg.configHome}/cava".source =
-      config.lib.file.mkOutOfStoreSymlink ./dotfiles/cava;
-    "${config.xdg.configHome}/wezterm".source =
-      config.lib.file.mkOutOfStoreSymlink ./dotfiles/wezterm;
-    "${config.xdg.configHome}/rofi".source =
-      config.lib.file.mkOutOfStoreSymlink ./dotfiles/rofi;
-    "${config.xdg.configHome}/swappy".source =
-      config.lib.file.mkOutOfStoreSymlink ./dotfiles/swappy;
+    "${config.xdg.configHome}/nvim".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/nvim;
+    "${config.xdg.configHome}/cava".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/cava;
+    "${config.xdg.configHome}/wezterm".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/wezterm;
+    "${config.xdg.configHome}/rofi".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/rofi;
+    "${config.xdg.configHome}/swappy".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/swappy;
 
-    "${config.xdg.configHome}/hypr".source =
-      config.lib.file.mkOutOfStoreSymlink ./dotfiles/hypr;
+    "${config.xdg.configHome}/hypr".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/hypr;
     "${config.xdg.configHome}/starship.toml".source =
       config.lib.file.mkOutOfStoreSymlink ./dotfiles/starship.toml;
-    "${config.xdg.configHome}/yazi".source =
-      config.lib.file.mkOutOfStoreSymlink ./dotfiles/yazi;
-
+    "${config.xdg.configHome}/yazi".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/yazi;
+    "${config.xdg.configHome}/fastfetch".source =
+      config.lib.file.mkOutOfStoreSymlink ./dotfiles/fastfetch;
   };
 }

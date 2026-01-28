806b9ff179668972d9f1a88bb83636250477504d
doyouwantto2 <tinhphong2580@gmail.com>
2026-01-28 13:24:42 +0700

    update

--- nixos/system/desktop/backend.nix
+++ nixos/system/desktop/backend.nix
@@ -11,19 +11,17 @@
   services.power-profiles-daemon.enable = true;
   services.upower.enable = true;
 
-  # Hyprland plugins configuration using native NixOS packages
-  # Available plugins from nixpkgs:
-  # - borders-plus-plus -> adds one or two additional borders to windows
-  # - hyprbars -> adds title bars to windows
-  # - hyprfocus -> flashfocus for hyprland
-  # - hyprscrolling -> adds a scrolling layout to hyprland
-  # - hyprspace -> workspace overview plugin for hyprland
-  # - hyprsplit -> plugin for awesome/dwm like workspaces
-  # - hyprtrails -> adds smooth trails behind moving windows
-  # - hyprwinwrap -> clone of xwinwrap, allows you to put any app as a wallpaper
-  # - xtra-dispatchers -> adds some new dispatchers
-  #
-  # Enabled plugins:
+  environment.sessionVariables = {
+    HYPR_PLUGIN_DIR = pkgs.symlinkJoin {
+      name = "hyprland-plugins";
+      paths = with pkgs.hyprlandPlugins; [
+        hy3
+        hyprspace
+      ];
+    };
+  };
+
+  # System packages including Hyprland plugins
   environment.systemPackages = with pkgs; [
     cava
     swappy
@@ -36,5 +34,6 @@
     fastfetch
 
     hyprlandPlugins.hy3
+    hyprlandPlugins.hyprspace # Temporarily disabled due to build issues
   ];
 }

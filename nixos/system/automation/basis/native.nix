64b6d9b5f920df10a969eceff7fedab23f6a1a74
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-03-01 23:28:22 +0700

    update

--- nixos/system/automation/basis/native.nix
+++ nixos/system/automation/basis/native.nix
@@ -9,6 +9,34 @@
   services.logind.settings.Login.HandleLidSwitch = "ignore";
   services.dbus.enable = true;
 
+  # Boot animations
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "rings";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "spin" ];
+        })
+      ];
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+      "systemd.show_status=auto"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+
+  };
+
   fonts = {
     enableDefaultPackages = true;
 

4507be0d6e65174f63618820a1887e74f870df48
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-03-19 17:49:12 +0700

    update

--- nixos/system/automation/basis/native.nix
+++ nixos/system/automation/basis/native.nix
@@ -13,11 +13,11 @@
   boot = {
     plymouth = {
       enable = true;
-      theme = "spin";
+      theme = "pixels";
       themePackages = with pkgs; [
         # By default we would install all themes
         (adi1090x-plymouth-themes.override {
-          selected_themes = [ "spin" ];
+          selected_themes = [ "pixels" ];
         })
       ];
     };

b128ce75a707c9f77e9dbecf26eac8de675f3c18
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-03-21 10:27:18 +0700

    update

--- nixos/system/desktop/frontend.nix
+++ nixos/system/desktop/frontend.nix
@@ -8,13 +8,7 @@
   environment.systemPackages = with pkgs; [
     quickshell
     noctalia-shell
-    dms-shell
     starship
     papirus-icon-theme
   ];
-
-  programs.dms-shell = {
-    enable = true;
-    systemd.enable = true;
-  };
 }

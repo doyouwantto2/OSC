3183cee65761ed35724ac30c2e8300be418f161a
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-06-04 23:31:31 +0700

    add tailwind tools

--- nixos/user/storage/dotfiles/nvim/lua/plugins/lsp.lua
+++ nixos/user/storage/dotfiles/nvim/lua/plugins/lsp.lua
@@ -279,6 +279,13 @@ return {
     end,
   },
 
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-telescope/telescope.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
   ------------------------------------------------
   -- Typst Preview
   ------------------------------------------------

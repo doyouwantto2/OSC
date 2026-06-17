44a87dc537f92d366768b96b88baa9c9bfa63d3c
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-06-18 00:52:08 +0700

    update

--- nixos/system/automation/basis/native.nix
+++ nixos/system/automation/basis/native.nix
@@ -21,7 +21,7 @@
       themePackages = with pkgs; [
         # By default we would install all themes
         (adi1090x-plymouth-themes.override {
-          selected_themes = [ "deusex" ];
+          selected_themes = [ "Deus Ex" ];
         })
       ];
     };

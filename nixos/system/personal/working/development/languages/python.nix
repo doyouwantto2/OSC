2a7e1bcd2dfcad0749f780ad3649d4b2fed99681
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-08-05 01:19:32 +0700

    update

--- nixos/system/personal/working/development/languages/python.nix
+++ nixos/system/personal/working/development/languages/python.nix
@@ -1,40 +1,33 @@
-{
-  config,
-  pkgs,
-  ...
-}:
+{ config, pkgs, ... }:
 
 {
+  nixpkgs.config.packageOverrides = super: let self = super.pkgs; in {
+    python3 = self.python311;
+  };
+
   environment.systemPackages = with pkgs; [
     (python3.withPackages (
       ps: with ps; [
         datasets
         kaggle
-
         opencv-python-headless
-        tensorflow-lite
         pygame
-
         numpy
         pandas
         matplotlib
         scikit-learn
-
         torch
         torchvision
         torchaudio
         botorch
         ollama
-
         pydbus
         pyserial
         pygobject3
-
         fastapi
         fastapi-cli
       ]
     ))
-
     pyright
   ];
 }

578fe36112b2caab3b9f81dbba71bd58f92f6d06
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-08-05 11:18:33 +0700

    update

--- flake.nix
+++ flake.nix
@@ -23,16 +23,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    impermanence = {
-      url = "github:nix-community/impermanence";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     haumea = {
       url = "github:nix-community/haumea";
       inputs.nixpkgs.follows = "nixpkgs";
